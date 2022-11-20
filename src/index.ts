497066ae86fd8a8cfd805b267320787cc84ec376
HiMeNoWorks <himeno.workspace@gmail.com>
2022-11-20 14:46:06 +0700

    use mail service

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { logDebug } from './utilities/winston';
+import MailSenderService from '../src/services/mail.service';
+import { IMail } from './interface/mail';
 
 dotenv.config();
 const port = process.env.PORT;
@@ -8,9 +10,21 @@ const port = process.env.PORT;
 const app = express();
 app.get('/health', (req: Request, res: Response) => {
     res.send('working!');
-    logDebug("check health");
+    logDebug('check health');
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post('/sendmail', (req: Request, res: Response) => {
+    const mail: IMail = req.body;
+    console.log(mail);
+
+    const result = new MailSenderService().sendEmail(mail);
+    res.send(result);
 });
 
 app.listen(port, () => {
-    console.log(`Running... at http://localhost:${port} | environment: ${process.env.ENVIRONMENT}`);
+    console.log(
+        `Running... at http://localhost:${port} | environment: ${process.env.ENVIRONMENT}`
+    );
 });
